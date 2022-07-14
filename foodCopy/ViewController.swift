65156859d8940cbb5f0fd864a9ea0368542a0ba2
GoForWalk <atcgsp912@gmail.com>
2022-07-14 15:11:30 +0900

    7 / 14 Library 적용 - 실습 (Using SPM) 1. FSCalendar 2. IQKeyboardManager 3. SideMenu 4. TextFieldEffects
    Storyboard 분리 방법 연습

--- foodCopy/ViewController.swift
+++ foodCopy/ViewController.swift
@@ -12,6 +12,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let format = DateFormatter()
+        
+        format.dateFormat = "M월 d일, yy년 hh:mm:ss"
+        format.locale = Locale(identifier: "ko-KR")
+        
+        let result = format.string(from: Date())
+        print(result)
+        
+        let word = "3월 2일, 19년 12:00:00"
+        let dateResult = format.date(from: word)
+        
+        print(dateResult!)
+        
         print(Date().description + " ViewController" + #function)
     }
 
